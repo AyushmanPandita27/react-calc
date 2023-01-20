8aa9e44f75a1a9dad19979d7881af977f253579c
ayushman27 <ayushman27@gmail.com>
2023-01-20 16:14:56 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Calculator from './components/first.component';
 
-function App() {
+function App({}) {
   return (
     <div className="App">
-<h1>Calc without styles</h1>
+
 <Calculator/>
     </div>
   );
--- src/components/first.component.jsx
+++ src/components/first.component.jsx
@@ -4,6 +4,12 @@ const Calculator=()=>{
  
     const [state, setState] = useState("");
     const [ans, setAns] = useState("");
+    const [color, setColor]=useState("");
+    const [col, setCol]=useState("");
+    const textStyle = {
+        color: `${col}`
+    }
+    document.body.style.background=color;
        const handler= (e)=>{
              setState(state.concat(e.target.value));
     }
@@ -17,14 +23,19 @@ const Calculator=()=>{
             setState("error");
         }
     }
+    const dark = ()=>{
+        setColor("black");
+        setCol("white");
+    }
     return(
         <div>
-            <h1 className="display">ans: {ans}</h1>
+           <h1 style={textStyle}>Calc without styles</h1>
+            <h1 style={textStyle} className="display">ans: {ans}</h1>
             <div >
-                <h3>value: {state} </h3> 
+                <h3 style={textStyle}>value: {state} </h3> 
                 </div>
 
-                <div className="numbers" style={{color:"blue"}}>
+                <div className="numbers" >
             < input type = "button" onClick={handler} value="1" />
             < input type = "button" onClick={handler} value="2"/>
             < input type = "button" onClick={handler} value="3"/>
@@ -45,6 +56,9 @@ const Calculator=()=>{
             < input type = "button" onClick={result} value="="/>
             < input type = "button" onClick={clear} value="C"/>
             </div>
+            <div>
+                <button onClick={dark}>Dark Mode</button>
+                </div>
         </div>
     );
 }
